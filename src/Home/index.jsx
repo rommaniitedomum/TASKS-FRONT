faeee7aa261c72dfc2ba86050b61937d26fa0bd7
rommaniitedomum <qkrwns982@gmail.com>
2024-11-06 17:11:57 +0900

    first commit

---
+++ src/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const index = () => {
+  return (
+    <div className="page-section">
+      <Navbar menuIdx={0} />
+    </div>
+  );
+};
+
+export default index;
